e24a1b7845aea2b867cffd2431331111055a3d44
Deiverso-JM <Sanabria020799@outlook.es>
2022-11-29 23:16:58 -0500

    #MEJORA_DE_ALGORITMO

--- ALGORITMOS_2_SIMPLES/Ejercicios_propuestos/Eje_#10.py
+++ ALGORITMOS_2_SIMPLES/Ejercicios_propuestos/Eje_#10.py
@@ -1,10 +1,9 @@
 #LIST
-cont=0
 letra=str(input("Digite una letra \n"))
 for i in ("a","e","i","o","u","A","E","I","O","U"):
     if (letra==i):
         print("Es una bocal")
-        continue
-    cont=cont+1
-    if(cont==10):
-        print("No es una bocal")
+        
+        break;
+else:
+    print("No es una bocal")
