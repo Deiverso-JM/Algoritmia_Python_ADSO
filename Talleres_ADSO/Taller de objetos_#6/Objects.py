a2936d3f83764667e0064e06efd61aec86d72a37
Deiverso-JM <Sanabria020799@outlook.es>
2023-04-03 10:42:23 -0500

    #Objetos_2

---
+++ Talleres_ADSO/Taller de objetos_#6/Objects.py
@@ -0,0 +1,71 @@
+#Navagadores super-class 
+#Superclass - herencia con parametros
+class Navegadores:
+    
+    #Constructor
+    def __init__(self,autor,font_family,motorBusqueda,marketPlace):   
+        self.autor=autor
+        self.font_family=font_family
+        self.motorBusqueda=motorBusqueda
+        self.marketplace=marketPlace
+    
+    #METODOS DE UTILIDADES
+    def busqueda(self):
+        print("Buscando....")
+        
+    def verificacionConexion(self):
+        print("......verifica")
+        print(".....Utilidad")
+        
+    def errorNavegador(self):
+        print("File not found")
+
+
+class standNavigation(Navegadores):
+    def subclas(self):
+        print("Otra clase mas")
+
+#Instancioas de objetos con init
+safari=standNavigation("Pedro pineda","Cursiva","Bin","playStore")
+chorme=standNavigation("Andes perez","Roboto","Bin","operaStore")
+opera=standNavigation("Manzana quintero","Arial","Bin","operaStore")
+eagle=standNavigation("Samuel luque","Roboto","Bin","microsoftStore")
+
+
+
+#SALIDAS METODOS
+
+safari.verificacionConexion()
+eagle.busqueda()
+opera.errorNavegador()
+
+
+
+#Salidas
+print("Navegadores atributos: ")
+print("safari: ", safari.autor,safari.font_family,safari.motorBusqueda,safari.marketplace)
+print("chorme: ", chorme.autor,chorme.font_family,chorme.motorBusqueda,chorme.marketplace)
+print("opera: ", opera.autor,opera.font_family,opera.motorBusqueda,opera.marketplace)
+print("eagle: ", eagle.autor,eagle.font_family,eagle.motorBusqueda,eagle.marketplace)
+print("_-------------------------------------------_")
+
+
+#Clases de herencia sin init
+class tor():
+    Nombre="onion"
+    utilidad="Cebolla"
+    autor="Anonimos"
+    
+        
+class hijotor(tor):
+    Uso="Sub-terraneoweb"
+
+#instancia sin init
+objetos2=hijotor()
+
+#Herencia sin init
+print("Utilidad ", objetos2.Nombre, objetos2.Uso,objetos2.utilidad)
+
+
+
+
