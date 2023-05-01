40e96ba6c572905cb4da0d3ea89544ceee7dac7d
Deiverso-JM <Sanabria020799@outlook.es>
2023-05-01 02:03:37 -0500

    #Conexion_BD

--- Bases de datos/bd.py
+++ Bases de datos/bd.py
@@ -1,3 +1,5 @@
+#Instanciando o conectando a la BD
+
 import mysql.connector
 
 midb=mysql.connector.connect(
@@ -7,10 +9,44 @@ midb=mysql.connector.connect(
     database='Pruebas'
 )
 
+#asignando
 cursor=midb.cursor()
 
-cursor.execute('select * from Usuario')
+# Listar datos
+#cursor.execute('select * from Usuario')
+# Fetchall TODOS  --- Fetchone UNO SOLO
+# resultado= cursor.fetchall() 
+# print(resultado)
 
-resultado= cursor.fetchone()
+#Limites
 
-print(resultado)
+# cursor.execute('select * from Usuario limit 1')
+# Resultado = cursor.fetchall()
+# print(Resultado)
+
+#Ver deficiones de tablas
+#cursor.execute('show create table Usuario')
+
+
+#Insertando a la BD
+# sql = 'insert into Usuario (username, gmail, edad) values(%s, %s,%s)'
+# values = ('micorreo@correo.co.nz', 'nombreusuario', 45)
+
+
+
+#Actualizar los datos
+# sql =  'update usuario set username = %s where id = %s'
+# values = ('Pabloperes',5)
+# cursor.execute(sql,values)
+
+# midb.commit()
+
+#print(cursor.rowcount)
+
+
+#eliminar datos
+
+# sql = 'delete from usuario where id = %s'
+# values = (4,)
+# cursor.execute(sql,values)
+# midb.commit()
